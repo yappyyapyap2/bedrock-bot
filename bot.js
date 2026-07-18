f734d7f297931c1c7df52c1499b1e311e8afe828
yappyyapyap2 <avi.v.goel@gmail.com>
2026-07-18 15:52:36 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+const bedrock = require('bedrock-protocol')
+
+const client = bedrock.createClient({
+  host: "BedrockAether.aternos.me", // your Aternos IP
+  port: 53569,                   // Bedrock default port
+  username: "Online_Bot"            // any name you want
+})
+
+client.on('join', () => {
+  console.log("Bot joined the server!")
+})
+
+client.on('text', (packet) => {
+  console.log(`[CHAT] ${packet.message}`)
+})
+
+client.on('disconnect', (packet) => {
+  console.log("Disconnected:", packet)
+})
