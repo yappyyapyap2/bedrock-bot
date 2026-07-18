e591f7456385442bf5f505cfa5f8d0639e5c0f08
yappyyapyap2 <avi.v.goel@gmail.com>
2026-07-18 16:11:45 -0700

    Update bot.js
    included version number for aternos server this time

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@ const bedrock = require('bedrock-protocol')
 const client = bedrock.createClient({
   host: "BedrockAether.aternos.me", // your Aternos IP
   port: 53569,                   // Bedrock default port
-  username: "Online_Bot"            // any name you want
+  username: "Online_Bot",            // any name you want
+  version: "1.26.33.2"
 })
 
 client.on('join', () => {
